9b96f26036952264953eee4eef21809e9579bf1e
a-int <somemailforicloud@icloud.com>
2023-09-01 14:38:28 +0300

    add examples with timers

---
+++ src/timers13.c
@@ -0,0 +1,47 @@
+#include <freertos_study.h>
+#include <timers.h>
+#include "llcm.h"
+#include "portmacro.h"
+#include "usart.h"
+
+static TickType_t mainOneShotTimer = pdMS_TO_TICKS(3333);
+static TickType_t mainAutoReload = pdMS_TO_TICKS(1000);
+static uint8_t uiAutoreloadCound = 0;
+
+void vOneShotTimerTask(TimerHandle_t timer);
+void vAutoReloadTimerTask(TimerHandle_t timer);
+
+int main() {
+  initSW();
+  init_clock_100();
+  init_SysTick(1000);
+  init_usart2_115200();
+
+  TimerHandle_t xOneShotTimer = xTimerCreate("One shot", mainOneShotTimer, pdFALSE, 0, vOneShotTimerTask);
+  TimerHandle_t xAutoReloadTimer = xTimerCreate("Auto Reload", mainAutoReload, pdTRUE, 0, vAutoReloadTimerTask);
+
+  BaseType_t xOneShotTimerStarted = pdFALSE;
+  BaseType_t xAutoReloadTimerStarted = pdFALSE;
+
+  if ((xOneShotTimer != NULL) && (xAutoReloadTimer != NULL)) {
+    xOneShotTimerStarted = xTimerStart(xOneShotTimer, 0);
+    xAutoReloadTimerStarted = xTimerStart(xAutoReloadTimer, 0);
+  }
+  if ((xOneShotTimerStarted == pdPASS) && (xAutoReloadTimerStarted == pdPASS)) {
+    vTaskStartScheduler();
+  }
+
+  while (1) {}
+  return 0;
+}
+
+void vOneShotTimerTask(TimerHandle_t timer) {
+  usart_sendString("One shot is done\r\n");
+}
+
+void vAutoReloadTimerTask(TimerHandle_t timer) {
+  usart_sendString("Auto reloaded: ");
+  usart_sendByte(uiAutoreloadCound + '0');
+  uiAutoreloadCound++;
+  usart_sendString("\r\n");
+}
---
+++ src/timers14.c
@@ -0,0 +1,47 @@
+#include <freertos_study.h>
+#include <timers.h>
+#include "llcm.h"
+#include "portmacro.h"
+#include "usart.h"
+
+static TickType_t mainAutoReload1 = pdMS_TO_TICKS(1000);
+static TickType_t mainAutoReload2 = pdMS_TO_TICKS(1000);
+
+static const uint32_t timer1Id = 1;
+static const uint32_t timer2Id = 2;
+
+void vOneShotTimerTask(TimerHandle_t timer);
+void vAutoReloadTimerTask(TimerHandle_t timer);
+
+int main() {
+  initSW();
+  init_clock_100();
+  init_SysTick(1000);
+  init_usart2_115200();
+
+  TimerHandle_t xAutoReloadTimer1 = xTimerCreate("Auto Reload", mainAutoReload1, pdTRUE, timer1Id, vAutoReloadTimerTask);
+  TimerHandle_t xAutoReloadTimer2 = xTimerCreate("Auto Reload", mainAutoReload2, pdTRUE, timer2Id, vAutoReloadTimerTask);
+
+  BaseType_t xAutoReloadTimerStarted1 = pdFALSE;
+  BaseType_t xAutoReloadTimerStarted2 = pdFALSE;
+
+  if ((xAutoReloadTimer1 != NULL) && (xAutoReloadTimer2 != NULL)) {
+    xAutoReloadTimerStarted1 = xTimerStart(xAutoReloadTimer1, 0);
+    xAutoReloadTimerStarted2 = xTimerStart(xAutoReloadTimer2, 0);
+  }
+  if ((xAutoReloadTimerStarted1 == pdPASS) && (xAutoReloadTimerStarted2 == pdPASS)) {
+    vTaskStartScheduler();
+  }
+
+  while (1) {}
+  return 0;
+}
+
+void vAutoReloadTimerTask(TimerHandle_t timer) {
+  const uint32_t id = pvTimerGetTimerID(timer);
+  if(id == timer1Id){
+    usart_sendString("The timer 1 is done\r\n");
+  } else if(id == timer2Id){
+    usart_sendString("The timer 2 is done\r\n");
+  }
+}
